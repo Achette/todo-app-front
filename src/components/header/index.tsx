714ad944bf1520c7de361c4c521a09522df7153d
Achette <i.achette@live.com>
2025-11-20 21:31:11 -0300

    feat: adiciona elementos no header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,20 +1,37 @@
-import { Box, Flex, Heading, Text } from '@chakra-ui/react'
+import { Box, Flex, Text } from '@chakra-ui/react'
+import { ResumeCard } from '../card'
+import { filterTasks } from '@/utils'
+import { AddButton } from '../add-button'
 
-export const Header = () => {
+interface HeaderProps {
+  tasks: TaskResponse
+}
+export const Header = ({ tasks }: HeaderProps) => {
+  const { activeTasks, doneTasks } = filterTasks(tasks)
+
   return (
-    <Flex>
+    <Flex justifyContent="space-between" alignItems="center">
       <Box>
-        <Heading
+        <Text
           fontSize="40px"
-          fontWeight={700}
+          fontWeight={800}
           bg="linear-gradient(90deg, #4f46e5, #9333ea)"
           bgClip="text"
-
         >
           Minhas Tarefas
-        </Heading>
-        <Text color="gray400" marginTop="8px">Organize seu dia de forma inteligente</Text>
+        </Text>
+        <Text color="gray400" fontSize="18px" marginTop="-8px">
+          Organize seu dia de forma inteligente
+        </Text>
       </Box>
+
+   
+      <Flex gap="20px">
+        <ResumeCard taskCount={activeTasks.length} taskStatus="active" />
+        <ResumeCard taskCount={doneTasks.length} taskStatus="done" />
+      </Flex>
+
+      <AddButton />
     </Flex>
   )
 }
