a771f676871e1eeb2b338d2a7fadb0aa6c4016db
Achette <i.achette@live.com>
2025-11-23 11:39:42 -0300

    feat: adiciona tabs no dashboard

--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -1,10 +1,37 @@
-import { Box, Text } from "@chakra-ui/react"
+'use client'
+import { Box, Flex, Text } from '@chakra-ui/react'
+import { Tabs } from '../tabs'
+import { tabsTitle } from '../tabs/tabs'
+import { useState } from 'react'
 
-export const Dashboard = () => {
+interface DashTasks {
+  tasks: TaskResponse
+}
 
-    return (
-        <Box>
-            <Text color="gray400">Listas de tarefas aqui...</Text>
-        </Box>
-    )
+export const Dashboard = ({ tasks }: DashTasks) => {
+  const [activeTab, setActiveTab] = useState(tabsTitle[0])
+
+  return (
+    <Box>
+      <Flex gap="12px">
+        {tabsTitle.map((tab) => (
+          <Tabs
+            key={tab}
+            name={tab}
+            isActive={activeTab === tab}
+            onClick={() => setActiveTab(tab)}
+          />
+        ))}
+      </Flex>
+
+      {/* TODO: Refatorar a exibição das tasks */}
+      <Box>
+        {tasks.map((task) => (
+          <Text key={task.id} color="gray400">
+            {task.title}
+          </Text>
+        ))}
+      </Box>
+    </Box>
+  )
 }
