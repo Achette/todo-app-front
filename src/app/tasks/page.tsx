e8c2f9ced701254351088976cd69767ab79b20e0
Achette <i.achette@live.com>
2025-11-23 11:38:54 -0300

    refactor: melhora tratamento de erros, ajusta props do header e melhorias pontuais no código

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,21 +1,35 @@
 import { Box } from '@chakra-ui/react'
-import { Dashboard, Header } from '@/components'
+import { Container, Dashboard, Header } from '@/components'
 import { getAllTasks } from '@/services'
+import { filterTasks } from '@/utils'
 
 const TaskDashboard = async () => {
-  const tasks = await getAllTasks()
+  let tasks: TaskResponse
+
+  try {
+    tasks = await getAllTasks()
+  } catch (error: any) {
+    tasks = []
+    const errorMessage =
+      error instanceof Error ? error.message : 'Erro desconhecido'
+    console.error('Erro ao buscar tarefas:', errorMessage)
+  }
+
+  const { activeTasks, doneTasks } = filterTasks(tasks)
 
   return (
     <Box>
-      <Box bg="white200" borderBottom="1px solid #f3f4f6" height="auto">
-        <Box maxW="5xl" px={6} py={8} mx="auto">
-          <Header tasks={tasks} />
-        </Box>
+      <Box bg="white200" borderBottom="2px solid #f3f4f6" height="auto">
+        <Container>
+          <Header
+            counters={{ active: activeTasks.length, done: doneTasks.length }}
+          />
+        </Container>
       </Box>
 
-      <Box maxW="5xl" px={6} py={8} mx="auto">
-        <Dashboard />
-      </Box>
+      <Container>
+        <Dashboard tasks={tasks} />
+      </Container>
     </Box>
   )
 }
