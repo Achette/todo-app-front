439a3a8c8a0654b9b34fe236124f415d529be929
Achette <i.achette@live.com>
2025-11-23 11:36:22 -0300

    feat: cria container component para evitar repetição

---
+++ src/components/container/index.tsx
@@ -0,0 +1,8 @@
+// components/Container.tsx
+import { Box, BoxProps } from '@chakra-ui/react'
+
+export const Container = ({ children, ...props }: BoxProps) => (
+  <Box maxW="5xl" px={6} py={8} mx="auto" {...props}>
+    {children}
+  </Box>
+)
