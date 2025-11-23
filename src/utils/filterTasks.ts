d795d31a05ca30d53de1002d4779994f9cd31119
Achette <i.achette@live.com>
2025-11-23 11:34:13 -0300

    refactor: altera função de filtragem de tasks

--- src/utils/filterTasks.ts
+++ src/utils/filterTasks.ts
@@ -1,9 +1,13 @@
-export const filterTasks = (tasks: TaskResponse) => {
-  return tasks.reduce(
-    (acc, task) => {
-      task.completed ? acc.doneTasks.push(task) : acc.activeTasks.push(task)
-      return acc
-    },
-    { activeTasks: [] as TaskResponse, doneTasks: [] as TaskResponse }
-  )
+export const filterTasks = (tasks?: TaskResponse) => {
+  const initialValue = {
+    activeTasks: [] as TaskResponse,
+    doneTasks: [] as TaskResponse,
+  }
+
+  if (!tasks) return initialValue
+
+  return tasks.reduce((acc, task) => {
+    task.completed ? acc.doneTasks.push(task) : acc.activeTasks.push(task)
+    return acc
+  }, initialValue)
 }
