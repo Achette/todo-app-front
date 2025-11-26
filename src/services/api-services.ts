db93cb79eed458c15c5f6cd0e53ae0b52e7a042d
Achette <i.achette@live.com>
2025-11-25 21:08:41 -0300

    feat: cria service para alterar checkbox

--- src/services/api-services.ts
+++ src/services/api-services.ts
@@ -7,3 +7,12 @@ export const getAllTasks = async (): Promise<TaskResponse> => {
 
   return HttpClient.request<TaskResponse>(endpoint, { method: 'GET' })
 }
+
+export const toggleTaskCompletion  = async (id: number, completed: boolean) => {
+  const endpoint = `http://localhost:8080/tasks/${id}`
+
+  return HttpClient.request<TaskResponse>(endpoint, {
+    method: 'PATCH',
+    body: JSON.stringify({ completed: !completed, userId: 5 }),
+  })
+}
