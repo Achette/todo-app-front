61b4f4ace656ab39cbaa82f62885228be6837241
Achette <i.achette@live.com>
2025-11-25 21:09:41 -0300

    refactor: altera nome da função e corrige imports

--- src/components/TaskItem/toggleTask.ts
+++ src/components/TaskItem/toggleTask.ts
@@ -1,10 +1,10 @@
 'use server'
 
-import { onChangeCheckbox } from '@/services'
+import { toggleTaskCompletion  } from '@/services'
 import { revalidatePath } from 'next/cache'
 
 export const toggleTask = async (id: number, completed: boolean) => {
-  await onChangeCheckbox(id, completed)
+  await toggleTaskCompletion (id, completed)
 
   revalidatePath('/tasks')  // função do Nextjs, após a requisição, atualiza a rota '/tasks'
 }
