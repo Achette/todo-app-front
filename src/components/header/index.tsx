a76ffca9d355299a7b50ad4acc1346bf53c3d4e3
Achette <i.achette@live.com>
2025-11-30 22:07:52 -0300

    refactor: refatora header para ajustar as páginas: principal, add e edit

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,35 +1,59 @@
 import { Box, Flex, Text } from '@chakra-ui/react'
 import { ResumeCard } from '../card'
 import { AddButton } from '../add-button'
+import { HeaderDescription, HeaderTypeEnum } from '@/constants'
+import { Container } from '../container'
+import { ArrowBack } from '../arrow-back'
 
 interface HeaderProps {
-  counters: { active: number; done: number }
+  counters?: { active: number; done: number }
+  type?: HeaderTypeEnum
 }
-export const Header = ({ counters }: HeaderProps) => {
-  const { active, done } = counters
+export const Header = ({
+  counters,
+  type = HeaderTypeEnum.DEFAULT,
+}: HeaderProps) => {
+  let active, done
+
+  if (counters) {
+    active = counters.active
+    done = counters.done
+  }
+
+  const { title, description } = HeaderDescription[type]
+
+  const buttonType = type === HeaderTypeEnum.DEFAULT ? 'ADD' : 'SAVE'
 
   return (
-    <Flex justifyContent="space-between" alignItems="center">
-      <Box>
-        <Text
-          fontSize="40px"
-          fontWeight={800}
-          bg="linear-gradient(90deg, #4f46e5, #9333ea)"
-          bgClip="text"
-        >
-          Minhas Tarefas
-        </Text>
-        <Text color="gray400" fontSize="18px" marginTop="-8px">
-          Organize seu dia de forma inteligente
-        </Text>
-      </Box>
+    <Box bg="white200" borderBottom="2px solid #f3f4f6" height="auto">
+      <Container>
+        <Flex justifyContent="space-between" alignItems="center">
+          <Flex alignItems="center" gap="1rem">
+            {type !== HeaderTypeEnum.DEFAULT ? <ArrowBack /> : null}
 
-      <Flex gap="20px">
-        <ResumeCard taskCount={active} taskStatus="active" />
-        <ResumeCard taskCount={done} taskStatus="done" />
-      </Flex>
-
-      <AddButton />
-    </Flex>
+            <Box>
+              <Text
+                fontSize="40px"
+                fontWeight={800}
+                bg="linear-gradient(90deg, #4f46e5, #9333ea)"
+                bgClip="text"
+              >
+                {title}
+              </Text>
+              <Text color="gray400" fontSize="18px" marginTop="-8px">
+                {description}
+              </Text>
+            </Box>
+          </Flex>
+          {type === HeaderTypeEnum.DEFAULT ? (
+            <Flex gap="20px">
+              <ResumeCard taskCount={active} taskStatus="active" />
+              <ResumeCard taskCount={done} taskStatus="done" />
+            </Flex>
+          ) : null}
+          <AddButton type={buttonType} />
+        </Flex>
+      </Container>
+    </Box>
   )
 }
