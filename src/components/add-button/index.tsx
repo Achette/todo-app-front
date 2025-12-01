b195c78ef6eba9049666e436aeb53ecdd9c28cc0
Achette <i.achette@live.com>
2025-11-30 22:06:00 -0300

    feat: adiciona novas funcionalidades ao botão

--- src/components/add-button/index.tsx
+++ src/components/add-button/index.tsx
@@ -1,13 +1,26 @@
-import { Button } from '@chakra-ui/react'
-import { LuPlus } from 'react-icons/lu'
+import { Link as ChackraLink } from '@chakra-ui/react'
+import { ReactElement } from 'react'
+import { LuPlus, LuSave } from 'react-icons/lu'
 
-export const AddButton = () => {
+interface ButtonProps {
+  type: string
+}
+
+const BUTTON_TYPE: Record<string, { name: string; icon: ReactElement }> = {
+  ADD: { name: 'Adicionar', icon: <LuPlus /> },
+  SAVE: { name: 'Criar Tarefa', icon: <LuSave /> },
+}
+
+export const AddButton = ({ type }: ButtonProps) => {
+  const { name, icon } = BUTTON_TYPE[type]
+
   return (
-    <Button
+    <ChackraLink
+      href="/tasks/add"
       bg="linear-gradient(to right, #4f46e5, #9333ea)"
       height="60px"
       color="white200"
-      fontSize="24px"
+      fontSize="20px"
       padding="16px 24px"
       borderRadius="xl"
       fontWeight="500"
@@ -16,12 +29,13 @@ export const AddButton = () => {
       gap="8px"
       boxShadow="sm"
       transition="all 0.2s ease-in-out"
+      textDecoration="none"
       _hover={{
         bg: 'linear-gradient(to right, #4338ca, #7e22ce)',
       }}
     >
-      <LuPlus />
-      Adicionar
-    </Button>
+      {icon}
+      {name}
+    </ChackraLink>
   )
 }
