631b522cffa01db96f3f2dc6ed33aec7ad85a020
Achette <i.achette@live.com>
2025-12-07 17:09:54 -0300

    refactor: adiciona nova props para reaproveirar component

--- src/components/priorityLabel/index.tsx
+++ src/components/priorityLabel/index.tsx
@@ -1,23 +1,31 @@
 import { Box, Flex, Text } from '@chakra-ui/react'
 
 interface PriorityLabelProps {
-  priority: 'HIGH' | 'MEDIUM' | 'LOW'
+  priority: Priority
+  size?: string
 }
 
 const PRIORITY_LABEL: Record<string, { bg: string }> = {
-  HIGH: { bg: 'red500' },
-  MEDIUM: { bg: 'yellow500' },
-  LOW: { bg: 'green500' },
+  ALTA: { bg: 'red500' },
+  MEDIA: { bg: 'yellow500' },
+  BAIXA: { bg: 'green500' },
 }
 
-export const PriorityLabel = ({ priority }: PriorityLabelProps) => {
+export const PriorityLabel = ({ priority, size }: PriorityLabelProps) => {
   const { bg } = PRIORITY_LABEL[priority]
   return (
     <Flex alignItems="center" gap="6px">
-      <Box width="8px" height="8px" borderRadius="full" bg={bg}></Box>
-      <Text color="gray400" fontSize="12px">
-        {priority.charAt(0).toUpperCase() + priority.slice(1).toLowerCase()}
-      </Text>
+      <Box
+        width={size ?? '8px'}
+        height={size ?? '8px'}
+        borderRadius="full"
+        bg={bg}
+      ></Box>
+      {!size ? (
+        <Text color="gray400" fontSize="12px">
+          {priority.charAt(0).toUpperCase() + priority.slice(1).toLowerCase()}
+        </Text>
+      ) : null}
     </Flex>
   )
 }
