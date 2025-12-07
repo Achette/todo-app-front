5b786758cb1eac2d1e77525355f6395c282abded
Achette <i.achette@live.com>
2025-12-07 17:11:07 -0300

    feat: cria enum para priority

---
+++ src/constants/priorityEnum.ts
@@ -0,0 +1,7 @@
+export enum PriorityEnum {
+  ALTA = 'Alta',
+  MEDIA = 'Média',
+  BAIXA = 'Baixa',
+}
+
+export const priorityLabelTab = [PriorityEnum.ALTA, PriorityEnum.MEDIA, PriorityEnum.BAIXA]
