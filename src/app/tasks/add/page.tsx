dd163c013e3f2628d50be48b32f5d1dd546693cd
Achette <i.achette@live.com>
2025-12-13 19:36:04 -0300

    style: move estilos para componente filho

--- src/app/tasks/add/page.tsx
+++ src/app/tasks/add/page.tsx
@@ -1,4 +1,3 @@
-import { Box } from '@chakra-ui/react'
 import { Container, Header, TaskForm } from '@/components'
 import { HeaderTypeEnum } from '@/constants'
 import { handleSubmitForm } from './action'
@@ -8,17 +7,7 @@ const AddTask = async () => {
     <>
       <Header type={HeaderTypeEnum.ADD} />
       <Container>
-        <Box
-          bg="white"
-          borderRadius="2xl"
-          shadow="2px 2px 8px rgba(0, 0, 0, 0.2)"
-          border="2px"
-          borderColor="gray.100"
-          overflow="hidden"
-          p="32px"
-        >
-          <TaskForm onSubmitForm={handleSubmitForm} />
-        </Box>
+        <TaskForm onSubmitForm={handleSubmitForm} />
       </Container>
     </>
   )
