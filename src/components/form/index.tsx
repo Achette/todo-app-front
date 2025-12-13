c555c010e81cdfdc20b2a583c2ef3ebbf4b54b3e
Achette <i.achette@live.com>
2025-12-13 17:42:55 -0300

    refactor: captura dados dos inputs no client para task preview

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,36 +1,74 @@
 'use client'
-import { useState } from 'react'
-import { Box, Field, Flex, Text, Separator } from '@chakra-ui/react'
+import { ChangeEvent, useCallback, useState } from 'react'
+import { Field, Flex, Text, Separator } from '@chakra-ui/react'
 import { PriorityLabelForm } from '../priorityLabel/priority-label-form'
-import { FormLabelEnum, priorityLabelTab } from '@/constants'
+import { FormLabelEnum, PriorityEnum, priorityLabelTab } from '@/constants'
 import { FormLabel } from './form-label'
 import { FormField } from './form-input'
 import { Calendar } from '../calendar'
+import { FormButton } from './form-button'
+import { useRouter } from 'next/navigation'
 
-export const TaskForm = () => {
-  const [activePriorityLabel, setActivePriorityLabel] = useState(
-    priorityLabelTab[1]
-  )
+interface TaskFormProps {
+  onSubmitForm: (formData: FormData) => Promise<void>
+}
+
+export const TaskForm = ({ onSubmitForm }: TaskFormProps) => {
+  const router = useRouter()
+
+  const [formInputs, setFormInputs] = useState({
+    title: '',
+    description: '',
+    priority: priorityLabelTab[1],
+    dueDate: '',
+  })
+
+  const handleGetFormInputs = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target
+    setFormInputs({ ...formInputs, [name]: value })
+  }
+
+  const handlePriorityChange = (label: 'Alta' | 'Média' | 'Baixa') => {
+    setFormInputs({ ...formInputs, priority: label })
+  }
+
+  const handleDateChange = useCallback((date: string) => {
+    setFormInputs((prev) => ({ ...prev, dueDate: date }))
+  }, [])
+
+  const handleSubmit = async (formData: FormData) => {
+    formData.append('priority', formInputs.priority) // adiciona um append ao form e captura o valor do label de prioridade
+    await onSubmitForm(formData)
+    router.back()
+  }
 
   return (
-    <Box as="form">
+    <form action={handleSubmit}>
       {/* Titulo */}
       <Field.Root>
         <FormLabel fieldType={FormLabelEnum.TITLE} />
-        <FormField placeholder="Digite o título da tarefa..." />
+        <FormField
+          placeholder="Digite o título da tarefa..."
+          value={formInputs.title}
+          setValue={handleGetFormInputs}
+        />
       </Field.Root>
 
       {/* Descrição */}
       <Field.Root marginTop="24px">
         <FormLabel fieldType={FormLabelEnum.DESCRIPTION} />
         <FormField
+          value={formInputs.description}
+          setValue={handleGetFormInputs}
           as="textarea"
           placeholder="Adicione uma descrição detalhada..."
           height="180px"
         />
         <Text color="gray400" fontSize="12px">
-          0 caracteres
-        </Text>{' '}
+          {formInputs.description.length} caracteres
+        </Text>
         {/* TODO: deixar dinâmico */}
       </Field.Root>
 
@@ -43,8 +81,8 @@ export const TaskForm = () => {
             <PriorityLabelForm
               key={label}
               name={label}
-              isActive={activePriorityLabel === label}
-              onClick={() => setActivePriorityLabel(label)}
+              isActive={formInputs.priority === label}
+              onClick={() => handlePriorityChange(label)}
             />
           ))}
         </Flex>
@@ -52,16 +90,19 @@ export const TaskForm = () => {
         {/* Data Vencimento */}
         <Flex flex={1} flexDirection="column">
           <FormLabel fieldType={FormLabelEnum.DUE_DATE} />
-          <Calendar />
+          <Calendar onDateChange={handleDateChange} />
         </Flex>
       </Flex>
 
+      <Flex flexDir="column" mb="1.5rem" alignItems="center">
+        <FormButton />
+      </Flex>
       <Separator borderColor="gray.100" opacity={1} />
 
       <Text color="gray400" fontSize="14px" textAlign="center" pt="24px">
         💡 Dica: Adicione uma descrição detalhada para não esquecer nenhum
         detalhe importante!
       </Text>
-    </Box>
+    </form>
   )
 }
