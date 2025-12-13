08598a755ceca890e71adf8b02ce0a9880c13031
Achette <i.achette@live.com>
2025-12-13 17:37:41 -0300

    cria server action para salvar formulário

---
+++ src/app/tasks/add/action.ts
@@ -0,0 +1,19 @@
+'use server'
+import { createNewTask } from '@/services'
+import { revalidatePath } from 'next/cache'
+import { normalizeForSave } from '@/utils'
+
+export async function handleSubmitForm(formData: FormData) {
+  const title = formData.get('title') as string
+  const description = formData.get('description') as string
+
+  const priority = formData.get('priority') as string
+  const normalizedPriority = normalizeForSave(priority)
+
+  const date = formData.get('dueDate') as string
+  const dueDate = new Date(date + 'T00:00:00').toISOString()
+
+  await createNewTask(title, description, normalizedPriority, dueDate)
+
+  revalidatePath('/tasks')
+}
