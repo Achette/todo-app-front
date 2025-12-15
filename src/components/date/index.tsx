f5f123585baede4b96b87ecb54b7e28026b87b99
Achette <i.achette@live.com>
2025-12-15 07:52:52 -0300

    feat: adiciona novas informaçoes de data, e contador de dias

--- src/components/date/index.tsx
+++ src/components/date/index.tsx
@@ -1,18 +1,60 @@
-import { Flex } from '@chakra-ui/react'
+import { Box, Flex, Text } from '@chakra-ui/react'
 import { LuCalendar } from 'react-icons/lu'
+import { dateFormatter, getDaysLabel } from '@/utils'
+import { PriorityLabel } from '../priorityLabel'
 
 interface DateProps {
   timestamp: string
+  dueDate: string
+  priority: Priority
+  completed: boolean
 }
 
-export const DateFormatter = ({ timestamp }: DateProps) => {
-  const date = new Date(timestamp)
-  const formattedDate = date.toLocaleDateString('pt-Br')
+export const DateAndPriority = ({
+  timestamp,
+  dueDate,
+  priority,
+  completed,
+}: DateProps) => {
+  const { formattedCreatedAt, formattedDueDate, diffDays } = dateFormatter(
+    timestamp,
+    dueDate
+  )
+
+  const { label, color, bg } = getDaysLabel(diffDays)
 
   return (
-    <Flex as="p" alignItems="center" gap="4px" color="gray400" fontSize="12px">
-      <LuCalendar />
-      {formattedDate}
+    <Flex w="100%" justifyContent="space-between">
+      <Flex
+        as="p"
+        alignItems="center"
+        gap="4px"
+        color="gray400"
+        fontSize="12px"
+      >
+        <Box
+          as="span"
+          display="inline-block"
+          w="1.5"
+          h="1.5"
+          borderRadius="full"
+          bg="gray.400"
+        />
+        <Text>Criada</Text>
+        {formattedCreatedAt} &nbsp;
+        <Text>Vence</Text>
+        <LuCalendar />
+        {formattedDueDate} &nbsp;
+        <PriorityLabel priority={priority} />
+      </Flex>
+
+      {!completed ? (
+        <Box bg={bg} p="6px 12px" borderRadius="8px">
+          <Text color={color} fontSize="12px" fontWeight={600}>
+            {label}
+          </Text>
+        </Box>
+      ) : null}
     </Flex>
   )
 }
