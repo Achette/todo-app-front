f99d3e7634f787a9448dd3c5e5ab80cc5460ced3
Achette <i.achette@live.com>
2025-12-15 07:51:29 -0300

    style: adiciona novos tokens de cor

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,4 +1,3 @@
-// theme.ts
 import { createSystem, defaultConfig, defineConfig } from '@chakra-ui/react'
 
 const config = defineConfig({
@@ -7,19 +6,29 @@ const config = defineConfig({
       colors: {
         bgActive: { value: '#EEF2FF' },
         bgDone: { value: '#F0FDF4' },
+        bgBlue50: { value: '#EFF6FF' },
+        bgBray50: { value: '#F9FAFB' },
+        bgOrange50: { value: '#FFF7ED' },
+        bgRed50: { value: '#FEF2F2' },
+        bgYellow50: { value: '#FEFCE8' },
         black600: { value: '#1F2937' },
+        blue600: { value: '#2563EB' },
         emerald500: { value: '#10B981' },
         gray400: { value: '#6B7280' },
+        gray600: { value: '#4B5563' },
         gray800: { value: '#4B5563' },
         green400: { value: '#16A34A' },
         green500: { value: '#22C552' },
         indigo100: { value: '#E0E7FF' },
         indigo800: { value: '#4F46E5' },
+        orange600: { value: '#EA580C' },
         purple600: { value: '#9333EA' },
         red500: { value: '#EF4444' },
+        red600: { value: '#DC2626' },
         white200: { value: '#F9F9F9' },
         white1000: { value: '#FFFFFF' },
         yellow500: { value: '#EAB308' },
+        yellow600: { value: '#CA8A04' },
       },
       fonts: {
         body: { value: 'Elms Sans, sans-serif' },
