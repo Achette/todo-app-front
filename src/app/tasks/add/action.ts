687d792b4061d2476196071d820009afca82960d
Achette <i.achette@live.com>
2025-12-21 20:43:38 -0300

    feat: ajusta action para criar nova tarefa

--- src/app/tasks/add/action.ts
+++ src/app/tasks/add/action.ts
@@ -1,25 +1,17 @@
 'use server'
 import { createNewTask } from '@/services'
 import { revalidatePath } from 'next/cache'
-import { normalizeForSave } from '@/utils'
+import { getFormData } from '@/utils'
 
 export async function handleSubmitForm(formData: FormData) {
-  const title = formData.get('title') as string
-  const description = formData.get('description') as string
-
-  const priority = formData.get('priority') as string
-  const normalizedPriority = normalizeForSave(priority)
-
-  const date = formData.get('dueDate') as string
-  const dueDate = new Date(date + 'T00:00:00').toISOString()
-
-  const createdAd = new Date().toISOString()
+  const { title, description, normalizedPriority, createdAt, dueDate } =
+    getFormData(formData)
 
   await createNewTask(
     title,
     description,
     normalizedPriority,
-    createdAd,
+    createdAt,
     dueDate
   )
 
