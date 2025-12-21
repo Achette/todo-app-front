fa19bc626ec3fc1afa414a8135477c5a0932894b
Achette <i.achette@live.com>
2025-12-21 20:47:56 -0300

    feat: adiciona nova props e services ao clicar nos botoes

--- src/components/icon-button/index.tsx
+++ src/components/icon-button/index.tsx
@@ -1,9 +1,11 @@
 import { Button } from '@chakra-ui/react'
+import { useRouter } from 'next/navigation'
 import { ReactElement } from 'react'
 import { LuPencilLine, LuTrash2 } from 'react-icons/lu'
 
 interface ButtonProps {
   type: string
+  id: number
 }
 
 const BUTTON_TYPE: Record<
@@ -24,11 +26,23 @@ const BUTTON_TYPE: Record<
   },
 }
 
-export const IconButton = ({ type }: ButtonProps) => {
+export const IconButton = ({ type, id }: ButtonProps) => {
   const { bg, color, icon, name } = BUTTON_TYPE[type]
+  const router = useRouter()
+
+  const handleEditTask = (id: number) => {
+    router.push(`/tasks/edit/${id}`)
+  }
+
+  const handleDeleteTask = (id: number) => {
+    console.log('Cliquei para apagar a tarefa: ', id)
+  }
+
+  const selectedButtonAction = type === 'EDIT' ? handleEditTask : handleDeleteTask 
 
   return (
     <Button
+      onClick={() => selectedButtonAction(id)}
       flexShrink={0}
       color="gray.400"
       transition="colors 0.2s"
