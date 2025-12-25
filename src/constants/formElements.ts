d540a18ae3e8840ac070351909dc23e1d22262cb
Achette <i.achette@live.com>
2025-12-25 19:36:35 -0300

    feat: cria const para remover magic strings do formulario

---
+++ src/constants/formElements.ts
@@ -0,0 +1,15 @@
+export const FormElements = {
+  editMode: 'edit',
+  createMode: 'create',
+  ariaLabel: 'Formulário de criação de tarefa',
+  title: 'title',
+  titlePlaceholder: 'Digite o título da tarefa...',
+  titleRequired: 'Título é obrigatório',
+  titleErrorMessage: 'Título não pode ser menor que 3 caracteres',
+  description: 'description',
+  descriptionPlaceholder: 'Adicione uma descrição detalhada...',
+  descriptionRequired: 'Descrição é obrigatório.',
+  descriptionErrorMessage: 'Descrição não pode ser menor que 10 caracteres',
+  priority: 'priority',
+  dueDate: 'dueDate',
+} as const
