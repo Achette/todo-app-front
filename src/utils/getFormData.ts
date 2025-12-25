4abe2189ba0e14d3d7f288c0d12cad85d5b84b6c
Achette <i.achette@live.com>
2025-12-25 19:51:25 -0300

    refactor: remove magic strings e adiciona verificacao para due-date

--- src/utils/getFormData.ts
+++ src/utils/getFormData.ts
@@ -1,16 +1,20 @@
+import { FormElements } from '@/constants'
 import { normalizeForSave } from './normalize'
 
 export const getFormData = (formData: FormData) => {
-  const title = formData.get('title') as string
-  const description = formData.get('description') as string
+  const title = formData.get(FormElements.title) as string
+  const description = formData.get(FormElements.description) as string
 
-  const priority = formData.get('priority') as string
+  const priority = formData.get(FormElements.priority) as string
   const normalizedPriority = normalizeForSave(priority)
 
-  const createdAt = new Date().toISOString()
+  const createdAtDate = new Date()
+  const createdAt = createdAtDate.toISOString()
 
-  const date = formData.get('dueDate') as string
-  const dueDate = new Date(date + 'T00:00:00').toISOString()
+  const date = formData.get(FormElements.dueDate) as string
+  const dueDate = date
+    ? new Date(date + 'T00:00:00').toISOString()
+    : new Date(createdAtDate.setDate(createdAtDate.getDate() + 1)).toISOString()
 
   return { title, description, normalizedPriority, createdAt, dueDate }
 }
