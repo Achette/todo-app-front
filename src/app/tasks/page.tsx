8d0397b62fcbfbc73541c36fe888da8eab09b65b
Achette <i.achette@live.com>
2026-01-14 22:38:10 -0300

    feat: adiciona info quando não há tasks

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,7 +1,9 @@
-import { Box, Text } from '@chakra-ui/react'
+import { Box, Flex, Text } from '@chakra-ui/react'
 import { Dashboard, Header } from '@/components'
 import { getAllTasks } from '@/services'
 import { filterTasks } from '@/utils'
+
+export const dynamic = 'force-dynamic'
 
 const TaskDashboard = async () => {
   let tasks: TaskResponse[]
@@ -14,20 +16,31 @@ const TaskDashboard = async () => {
       error instanceof Error ? error.message : 'Erro desconhecido'
     console.error('Erro ao buscar tarefas:', errorMessage)
   }
+  const hasTasks = tasks.length > 0
 
   const { activeTasks, doneTasks } = filterTasks(tasks)
 
   return (
-    <Box>
+    <Box height="100vh">
       <Header
         counters={{ active: activeTasks.length, done: doneTasks.length }}
       />
 
-      <Dashboard tasks={tasks} />
+      {hasTasks ? (
+        <>
+          <Dashboard tasks={tasks} />
 
-      <Text color="gray400" textAlign="center" marginTop="-0.5rem">
-        {doneTasks.length} de {tasks.length} tarefas concluídas
-      </Text>
+          <Text color="gray400" textAlign="center" marginTop="-0.5rem">
+            {doneTasks.length} de {tasks.length} tarefas concluídas
+          </Text>
+        </>
+      ) : (
+        <Flex h="70%" justifyContent="center" alignItems="center">
+          <Text fontSize="xl" color="gray400" fontWeight={600}>
+            Não há tarefas no seu board 😔
+          </Text>
+        </Flex>
+      )}
     </Box>
   )
 }
